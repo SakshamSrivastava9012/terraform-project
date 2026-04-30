097f8bbc98130d1ff4f9d97360de1af89acaf979
Saksham Srivastava <sakshamsrivastava9012@gmail.com>
2026-04-30 14:13:31 +0530

    bt mistake

--- terraform-project/modules/ec2/main.tf
+++ terraform-project/modules/ec2/main.tf
@@ -22,50 +22,24 @@ resource "aws_security_group" "ec2sgroup" {
   }
 }
 
-resource "aws_launch_template" "webtestec2" {
-
-  image_id      = var.amiid
+resource "aws_instance" "webtestec2" {
+  ami           = var.amiid
   instance_type = var.instancetype
 
-  user_data = base64encode(<<-EOF
+  vpc_security_group_ids = [aws_security_group.ec2sgroup.id]
+
+  user_data = <<-EOF
     #!/bin/bash
     yum update -y
     yum install -y httpd
     systemctl start httpd
     systemctl enable httpd
-    
-    echo "<h1> hello world</h2>" > /var/www/html/index.html
+
+    echo "<h1> hello world</h1>" > /var/www/html/index.html
   EOF
-  )
 
-  network_interfaces {
-    security_groups = [aws_security_group.ec2sgroup.id]
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "sakshamwebserver"
-    }
+  tags = {
+    Name = "sakshamwebserver"
   }
 }
 
-resource "aws_autoscaling_group" "webtestec2" {
-  name                = "sakshamasg"
-  vpc_zone_identifier = var.privatesubnetids
-  desired_capacity    = var.desiredcapacity
-  max_size            = var.desiredcapacity
-  min_size            = var.desiredcapacity
-  target_group_arns   = [var.targetgrouparn]
-
-  launch_template {
-    id      = aws_launch_template.webtestec2.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "sakshamasginstance"
-    propagate_at_launch = true
-  }
-}
